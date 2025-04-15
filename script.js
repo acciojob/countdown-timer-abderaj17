c873eb9bba0d97951dc1eb1a2e0a9facfde1fd15
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-04-15 21:32:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 // Your script here.
+const inputField = document.getElementById('userInput');
+const remainingTimeDisplay = document.getElementById('countDown');
+const entTimeDisplay = document.getElementById('endTime');
+
